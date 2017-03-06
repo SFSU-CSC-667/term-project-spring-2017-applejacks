4cfebfb1e6080480311b565a76e83e9b03ca4aed
srbecker <sambecker88@gmail.com>
2017-03-05 18:19:02 -0800

    modify server and env port

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.post('/login', function (req, res) {
   res.send('posted');
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Server started on port ' + process.env.PORT);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started on port ' + port);
 });
