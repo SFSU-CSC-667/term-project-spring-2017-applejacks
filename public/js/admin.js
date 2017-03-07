359073c4bedeae72f38ab8eea5b71f8aa82f26b7
srbecker <sambecker88@gmail.com>
2017-03-07 01:51:33 -0800

    [admin] - update record button, ui only

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,10 +4,13 @@ dbTable.addEventListener('click', function (event) {
   var target = event.target,
     parent = target.parentNode,
     cellArr = [],
+    filteredArr = [],
     i = 0,
     docFrag,
     input,
-    td;
+    td,
+    node,
+    btn;
 
   if (parent.className !== 'record') {
     return;
@@ -21,21 +24,29 @@ dbTable.addEventListener('click', function (event) {
   console.log(parent);
 
   cellArr = Array.prototype.slice.call(parent.querySelectorAll('td'));
-  cellArr = cellArr.map(function (node) {
-    return node.textContent;
-  });
+  for (i = 0; i < cellArr.length; i++) {
+    node = cellArr[i];
+    
+    if (node.children.length === 0) {
+      filteredArr.push(node.textContent);
+    } 
+  }
 
   parent.classList.toggle('record-edit');
+  btn = parent.querySelector('.record-update');
+  btn.classList.toggle('hidden');
+  btn = btn.parentNode;
 
   docFrag = document.createDocumentFragment();
-  for (i = 0; i < cellArr.length; i++) {
+  for (i = 0; i < filteredArr.length; i++) {
     input = document.createElement('input');
-    td = document.createElement('td');
-    input.value = cellArr[i];
+    td = document.createElement('td');    
+    input.value = filteredArr[i];
     input.className = 'record-edit-field';
     td.appendChild(input);
     docFrag.appendChild(td);
-  }
+  }  
+  docFrag.appendChild(btn);
 
   parent.innerHTML = '';
   parent.appendChild(docFrag);
