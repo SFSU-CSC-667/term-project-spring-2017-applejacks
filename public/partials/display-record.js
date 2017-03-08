4759e4353ce42f6725697b5a5c66129886df0a71
srbecker <sambecker88@gmail.com>
2017-03-07 23:48:55 -0800

    [arch][build] - changing template file name

--- public/partials/display-record.js
+++
@@ -1,8 +0,0 @@
-<script id="display-record" type="text/x-handlebars-template">
-  <div class="entry">
-    <h1>{{title}}</h1>
-    <div class="body">
-      {{body}}
-    </div>
-  </div>
-</script>
