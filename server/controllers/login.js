01105112c23923e88e3f6585afcc292bd689c835
Sam Becker <sbecker@linkedin.com>
2017-03-07 18:50:04 -0800

    [arch][encryption] - temporary branch until bcrypt is added

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,10 +1,16 @@
 /* login routes */
 var express      = require('express'),
   router         = express.Router(),
+  db             = require('./database').db, // ./database is the relative path
   printlog       = require('./../helpers').printlog;
 
 router.post('/', function (req, res) {
   // TODO: add middleware auth here
+  
+  // Load hash from your password DB. 
+  // bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+  //     // res == true 
+  // });
 
   // redirect to lobby after user has logged in
   res.render('lobby', {});
